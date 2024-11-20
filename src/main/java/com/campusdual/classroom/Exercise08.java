f3d00df5dac1027a3129a3cf9745ab9c7e4c6875
Gabriel <Gabrieldb050890@gmail.com>
2024-11-20 19:39:20 +0100

    Ejercicio hecho

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -3,7 +3,7 @@ package com.campusdual.classroom;
 public class Exercise08 {
 
     public static void main(String[] args) {
-        whileLoop(1);
+        whileLoop(20);
     }
 
     //TODO ↓
@@ -13,10 +13,9 @@ public class Exercise08 {
     // etc.
     public static void whileLoop(int num) {
         int x =0;
-        while (x < num && num <= 30) {
-             System.out.println(x +" < "+num+". El proximo ciclo valdra: "+(num+1));
+        while (x < num ) {
+             System.out.println(x +" < "+num+". El proximo ciclo valdra: "+(x+1));
             x++;
-            num++;
-        }
+                    }
     }
 }
